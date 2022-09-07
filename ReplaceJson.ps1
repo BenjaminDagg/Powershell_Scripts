bdef7cb9237546cd923a7903e9fe7b52f72aa6e7
unknown <bdagg@csumb.edu>
2022-09-06 22:31:33 -0700

    Replace json script

---
+++ ReplaceJson.ps1
@@ -0,0 +1,22 @@
+﻿
+$filePath =  "$(ExtractedPath)\TestConnectionString\POS\appsettings.json"
+$newConnectionString = "$(POSConnectionString)"
+
+if((Test-Path -Path $filePath -PathType Leaf)) {
+    
+    #remove comments from json
+    $file = (Get-Content $filePath -raw)
+    $file = $file -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/'
+
+    #convert to json
+    $json =  $file | ConvertFrom-Json
+
+    #replace connection string
+    $json.ConnectionStrings.ApplicationConnectionString = $newConnectionString
+    $json | ConvertTo-Json -depth 35 | Out-File $filePath
+    
+}
+else {
+    Write-Host "File $filePath not found"
+}
+
