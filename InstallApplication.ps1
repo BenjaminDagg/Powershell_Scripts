09cf771c308c6ef19b85cec58e1d45773fa245c9
unknown <bdagg@csumb.edu>
2022-09-07 21:01:17 -0700

    Added script to install TP

---
+++ InstallApplication.ps1
@@ -0,0 +1,27 @@
+﻿
+$path = "C:\Install\TPTest\OLG Lottery Version 4.0.6A\Retail\LotteryTransactionPortal\setup.exe"
+$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Transaction Portal"}
+
+if($app){
+    
+    Write-Host "Transaction Portal is installed. Uninstalling..."
+    $app.Uninstall()
+}
+else {
+    Write-Host "Software is not installed"
+}
+
+$service = Get-WmiObject -Class Win32_Service -Filter "Name='Transaction Portal'"
+if($service){
+    Write-Host "Service found"
+    Stop-Service -Name $service.Name
+
+    $service.delete()
+}
+
+#Install TP
+Start-Process -Wait -FilePath $path -ArgumentList "/quiet" -PassThru
+
+#start service
+$service = Get-Service "Transaction Portal"
+$service.start()
